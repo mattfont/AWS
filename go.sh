142dfcc6f5ba903d2510b9407e891a00c993f311
Matthew Fontenot <matthew_fontenot@comcast.com>
2019-03-04 11:46:36 -0600

    changed to use a ENV VAR for aws profile

--- go.sh
+++ go.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export AWS_DEFAULT_PROFILE=saml
+
 echo "Create bucket"
 echo
-aws s3 mb s3://mattstestbucketforclass2 --region us-west-2 --profile saml
+aws s3 mb s3://mattstestbucketforclass2 --region us-west-2
 echo
 echo "Create Files"
 echo > test1.txt
@@ -13,26 +15,26 @@ sleep 3
 
 echo "Copy files to bucket"
 echo
-aws s3 cp test1.txt s3://mattstestbucketforclass2 --profile saml
-aws s3 cp test2.txt s3://mattstestbucketforclass2 --profile saml
+aws s3 cp test1.txt s3://mattstestbucketforclass2 
+aws s3 cp test2.txt s3://mattstestbucketforclass2 
 echo
 sleep 3
 echo "list contents of bucket"
-aws s3 ls s3://mattstestbucketforclass2 --profile saml
+aws s3 ls s3://mattstestbucketforclass2 
 echo
 sleep 3
 echo "List buckets in us-west-2"
-aws s3 ls --region us-west-2 --profile saml
+aws s3 ls --region us-west-2
 echo
 sleep 3
 echo "List Policy of bucket"
-aws s3api get-bucket-policy --bucket mattstestbucketforclass2 --profile saml
+aws s3api get-bucket-policy --bucket mattstestbucketforclass2 
 echo
 sleep 3
 echo "Delete Files"
-aws s3 rm s3://mattstestbucketforclass2/test1.txt --profile saml
-aws s3 rm s3://mattstestbucketforclass2/test2.txt --profile saml
+aws s3 rm s3://mattstestbucketforclass2/test1.txt 
+aws s3 rm s3://mattstestbucketforclass2/test2.txt 
 echo
 echo "Delete Bucket"
-aws s3api delete-bucket --bucket mattstestbucketforclass2 --region us-west-2 --profile saml
+aws s3api delete-bucket --bucket mattstestbucketforclass2 --region us-west-2 
 
